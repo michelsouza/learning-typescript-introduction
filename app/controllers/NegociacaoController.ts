afa22b8a7b31dd8e4573dee50338dee1b48e0c63
Michel Souza <michelsouza.dev@gmail.com>
2023-03-16 01:00:30 -0300

    adicionando nova configuracao em tsconfig, alem de tipar todos os atributos e metodos das classes

--- app/controllers/NegociacaoController.ts
+++ app/controllers/NegociacaoController.ts
@@ -1,8 +1,10 @@
+import {Negociacao} from "../models/Negociacao.js";
+
 export class NegociacaoController {
 
-    private inputData;
-    private inputQuantidade;
-    private inputValue;
+    private inputData: HTMLInputElement; // tipo contigo no ts para elementos do tipo inport de form
+    private inputQuantidade: HTMLInputElement;
+    private inputValue: HTMLInputElement;
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -10,9 +12,28 @@ export class NegociacaoController {
         this.inputValue = document.querySelector('#valor');
     }
 
-    adiciona() {
-        console.log(this.inputData);
-        console.log(this.inputQuantidade);
-        console.log(this.inputValue);
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        this.limparFormulario();
+    }
+
+    criaNegociacao(): Negociacao {
+        /**
+         * mesmo inputData sendo declarado do tipo HTMLInputElment, por default, t-odo input retorna uma string
+         * logo, é preciso converter estes valores para seus respectivos, segundo sua declaracao
+         */
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const value = parseFloat(this.inputValue.value);
+
+        return new Negociacao(date, quantidade, value);
+    }
+
+    limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValue.value = '';
+        this.inputData.focus();
     }
 }
--- dist/js/controllers/NegociacaoController.js
+++ dist/js/controllers/NegociacaoController.js
@@ -1,3 +1,4 @@
+import { Negociacao } from "../models/Negociacao.js";
 export class NegociacaoController {
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -5,8 +6,24 @@ export class NegociacaoController {
         this.inputValue = document.querySelector('#valor');
     }
     adiciona() {
-        console.log(this.inputData);
-        console.log(this.inputQuantidade);
-        console.log(this.inputValue);
+        const negociacao = this.criaNegociacao();
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        /**
+         * mesmo inputData sendo declarado do tipo HTMLInputElment, por default, t-odo input retorna uma string
+         * logo, é preciso converter estes valores para seus respectivos, segundo sua declaracao
+         */
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const value = parseFloat(this.inputValue.value);
+        return new Negociacao(date, quantidade, value);
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValue.value = '';
+        this.inputData.focus();
     }
 }
