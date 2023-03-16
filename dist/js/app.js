58d764e2e56e54be158c2fac16f545ba7e87bc0a
Michel Souza <michelsouza.dev@gmail.com>
2023-03-16 00:00:45 -0300

    configurando alguns itens no typescript para que se crie um ambiente adequado para desenvolvimento, como exemplo, não criar os arquivos js na pasta dist caso haja algum erro na compilação do ts. Também alteramos os arquivos de dist para app, a qual conterá todo o conteudo ts

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,2 @@
-import { Negociacao } from "./models/Negociacao";
-
+import { Negociacao } from "./models/Negociacao.js";
 const negociacao = new Negociacao(new Date(), 10, 100);
-
-console.log(negociacao);
